e1a5c67a822f1b9b87d5280457a141cfd42e6c0f
Yngwarr <k.yngvarr@gmail.com>
2019-01-09 00:29:03 +0700

    Final (style sucks).

--- js/main.js
+++ js/main.js
@@ -39,13 +39,13 @@ let proff = [
 ];
 
 let course = [
-	'Иностранный язык',
-	'Объектно-ориентированные технологии разработки ПО',
-	'Философия',
-	'Основы теории машинного обучения',
-	'Современные технологии баз данных',
-	'Методы активной идентификации динамических систем',
-	'Модели временных рядов',
+	['Иностранный язык', 'Ин. яз.'],
+	['Объектно-ориентированные технологии разработки ПО', 'ООТРПО'],
+	['Философия', 'Фил.'],
+	['Основы теории машинного обучения', 'ОТМО'],
+	['Современные технологии баз данных', 'СТБД'],
+	['Методы активной идентификации динамических систем', 'МАИДС'],
+	['Модели временных рядов', 'МВР'],
 ];
 
 let sched = {
@@ -59,9 +59,17 @@ let sched = {
 	],
 };
 
+function short_p(name) {
+	let names = name.split(' ');
+	for (let i = 1; i < names.length; ++i) {
+		names[i] = `${names[i][0]}.`;
+	}
+	return names.join(' ');
+}
+
 /******** [/ACTUAL DATA] ********/
 
-function show(data) {
+function show_sched(data) {
 	let m = document.querySelector('main');
 	for (let d in data) {
 		d = parseInt(d);
@@ -75,27 +83,42 @@ function show(data) {
 		for (let i = 0; i < data[d].length; ++i) {
 			let datum = data[d][i];
 			let li = document.createElement('li');
+			if (datum[3]) {
+				if (datum[3].includes('ч')) li.classList.add('even');
+				if (datum[3].includes('н')) li.classList.add('odd');
+				if (datum[3].includes('л')) li.classList.add('lecture');
+			}
 
 			let time = document.createElement('span');
 			let [from, to] = times[datum[0]];
 			time.classList.add('time');
-			time.innerHTML = `${from} <span class='dash'>-</span> ${to}`;
+			time.innerHTML = `${from} <span class='dash'>&ndash;</span> ${to}`;
 
 			let c = document.createElement('span');
-			c.classList.add('course');
+			c.classList.add('course', 'full');
 			c.dataset.num = datum[2];
-			c.textContent = course[datum[2]];
+			c.textContent = course[datum[2]][0];
+			let sc = document.createElement('span');
+			sc.classList.add('course', 'short');
+			sc.dataset.num = datum[2];
+			sc.textContent = course[datum[2]][1];
 
 			let p = document.createElement('span');
-			p.classList.add('proff');
+			p.classList.add('proff', 'full');
 			p.dataset.num = datum[1];
 			p.textContent = proff[datum[1]];
+			let sp = document.createElement('span');
+			sp.classList.add('proff', 'short');
+			sp.dataset.num = datum[1];
+			sp.textContent = short_p(proff[datum[1]]);
 
 			li.appendChild(time);
 			li.appendChild(document.createTextNode(': '))
 			li.appendChild(c);
+			li.appendChild(sc);
 			li.appendChild(document.createTextNode(' ('))
 			li.appendChild(p);
+			li.appendChild(sp);
 			li.appendChild(document.createTextNode(')'))
 			ul.appendChild(li);
 		}
@@ -105,6 +128,66 @@ function show(data) {
 	}
 }
 
+function hide_week(even) {
+	if (even) {
+		document.querySelectorAll('.even').forEach(el => el.classList.remove('hidden'));
+		document.querySelectorAll('.odd').forEach(el => el.classList.add('hidden'));
+	} else {
+		document.querySelectorAll('.odd').forEach(el => el.classList.remove('hidden'));
+		document.querySelectorAll('.even').forEach(el => el.classList.add('hidden'));
+	}
+}
+
+function toggle_week(checked) {
+	let even = week_num % 2 === 0;
+	hide_week(even && !checked || !even && checked);
+}
+
+function shorten(what) {
+	let lc = document.getElementById('full-course').checked;
+	let lp = document.getElementById('full-proff').checked;
+	if (!what || what.includes('c')) {
+		document.querySelectorAll('.course.short').forEach(el => {
+			if (lc) el.classList.add('hidden');
+			else el.classList.remove('hidden');
+		});
+		document.querySelectorAll('.course.full').forEach(el => {
+			if (lc) el.classList.remove('hidden');
+			else el.classList.add('hidden');
+		});
+	}
+	if (!what || what.includes('p')) {
+		document.querySelectorAll('.proff.short').forEach(el => {
+			if (lp) el.classList.add('hidden');
+			else el.classList.remove('hidden');
+		});
+		document.querySelectorAll('.proff.full').forEach(el => {
+			if (lp) el.classList.remove('hidden');
+			else el.classList.add('hidden');
+		});
+	}
+	localStorage.sh = `${!lc ? 'c' : ''}${!lp ? 'p' : ''}`;
+}
+
+function up_date() {
+	document.getElementById('date').textContent = (new Date()).toLocaleDateString();
+}
+
+let week_num;
+
 function init() {
-	show(sched);
+	week_num = (new Date()).getWeek();
+	document.getElementById('week-num').textContent = week_num.toString();
+	up_date();
+	show_sched(sched);
+	hide_week(week_num % 2 === 0);
+	// TODO read and store flags in localStorage
+	if (!localStorage.sh) localStorage.sh = '';
+	if (localStorage.sh.includes('c')) {
+		document.getElementById('full-course').checked = false;
+	}
+	if (localStorage.sh.includes('p')) {
+		document.getElementById('full-proff').checked = false;
+	}
+	shorten();
 }
